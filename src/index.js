f5bd3b040324e9ac647130ea134cadaac28538df
iDenco <idenco90@gmail.com>
2017-11-16 11:07:18 +0700

    Add AddUser Component & Dockerfile

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom'
 import axios from 'axios'
 
 import UsersList from './components/UsersList'
+import AddUser from './components/AddUser'
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			users: []
+			users: [],
+			username: '',
+			email: ''
 		}
 	}
 	componentDidMount() {
@@ -24,6 +27,27 @@ class App extends Component {
 				console.log(err)
 			})
 	}
+	addUser(event) {
+		event.preventDefault()
+		const data = {
+			username: this.state.username,
+			email: this.state.email
+		}
+		axios
+			.post(`${process.env.REACT_APP_PAYROLL_SERVICE_URL}/users`, data)
+			.then(res => {
+				this.getUsers()
+				this.setState({ username: '', email: '' })
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}
+	handleInputChange(event) {
+		const obj = {}
+		obj[event.target.name] = event.target.value
+		this.setState(obj)
+	}
 	render() {
 		return (
 			<div className="container">
@@ -32,6 +56,13 @@ class App extends Component {
 						<br />
 						<h1>All Users</h1>
 						<hr />
+						<br />
+						<AddUser
+							username={this.state.username}
+							email={this.state.email}
+							handleInputChange={this.handleInputChange.bind(this)}
+							addUser={this.addUser.bind(this)}
+						/>
 						<br />
 						<UsersList users={this.state.users} />
 					</div>
